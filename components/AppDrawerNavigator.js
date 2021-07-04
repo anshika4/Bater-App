6164132fbeef457f0b53119a0328a9e315218531
anshika4 <72254585+anshika4@users.noreply.github.com>
2021-07-04 16:22:05 +0530

    Update AppDrawerNavigator.js

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -4,18 +4,39 @@ import { AppTabNavigator } from './AppTabNavigator'
 import CustomSideBarMenu  from './CustomSideBarMenu';
 import SettingScreen from '../screens/SettingScreen';
 import BarterScreen from '../screens/mybarters';
+import NotificationScreen from '../screens/NotificationsScreen'
+import {Icon} from 'react-native-elements';
+
 
 export const AppDrawerNavigator = createDrawerNavigator({
   Home : {
-    screen : AppTabNavigator
+    screen : AppTabNavigator,
+    navigationOptions:{
+      drawerIcon : <Icon name="home" type ="fontawesome5" />
+    }
     },
 
     MyBarters:{
       screen : BarterScreen,
+      navigationOptions:{
+      drawerIcon : <Icon name="gift" type ="font-awesome" />,
+      drawerLabel : "MyBarters"
+    }
     },
       Setting : {
-      screen : SettingScreen
+      screen : SettingScreen,
+       navigationOptions:{
+      drawerIcon : <Icon name="settings" type ="fontawesome5" />,
+      drawerLabel : "Settings"
     }
+    },
+    Notifications:{
+    screen:NotificationScreen,
+    navigationOptions:{
+      drawerIcon : <Icon name="bell" type ="font-awesome" />,
+      drawerLabel : "Notifications"
+    }
+  }
   },
   {
     contentComponent:CustomSideBarMenu
