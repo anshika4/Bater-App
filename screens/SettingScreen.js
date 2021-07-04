99abb751a9f394539f940aaa083fce0b11b57e8d
anshika4 <72254585+anshika4@users.noreply.github.com>
2021-07-04 16:13:48 +0530

    Update SettingScreen.js

--- screens/SettingScreen.js
+++ screens/SettingScreen.js
@@ -1,173 +1,206 @@
-import React,{Component}from 'react';
-import {View,Text,TextInput,KeyboardAvoidingView,StyleSheet,TouchableOpacity,Alert,} from 'react-native';
-import db from '../config';
-import firebase from 'firebase';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { Card } from "react-native-elements";
+import MyHeader from "../components/MyHeader";
+import db from "../config";
+import firebase from "firebase";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default class SettingScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      emailId: "",
+      firstName: "",
+      lastName: "",
+      address: "",
+      contact: "",
+      docId: "",
+    };
+  }
+
+  getUserDetails = () => {
+    var email = firebase.auth().currentUser.email;
+    db.collection("users")
+      .where("email_id", "==", email)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          var data = doc.data();
+          this.setState({
+            emailId: data.email_id,
+            firstName: data.first_name,
+            lastName: data.last_name,
+            address: data.address,
+            contact: data.contact,
+            docId: doc.id,
+          });
+        });
+      });
+  };
+
+  updateUserDetails = () => {
+    db.collection("users").doc(this.state.docId).update({
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      address: this.state.address,
+      contact: this.state.contact,
+    });
+
+    Alert.alert("Profile Updated Successfully");
+  };
+
+  componentDidMount() {
+    this.getUserDetails();
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 0.12 }}>
+          <MyHeader title="Settings" navigation={this.props.navigation} />
+        </View>
 
 
+        <View style={styles.formContainer}>
+            <View
+              style={{
+                flex: 0.66,
+                padding: RFValue(10),
+              }}
+            >
+            <Text style={styles.label}>First Name </Text>
+              <TextInput
+                style={styles.formTextInput}
+                placeholder={"First Name"}
+                maxLength={12}
+                onChangeText={(text) => {
+                  this.setState({
+                    firstName: text,
+                  });
+                }}
+                value={this.state.firstName}
+              />
 
-export default class SettingScreen extends Component{
-   constructor(){
-super();
-this.state={
-emailId:'',
-firstName:'',
-lastName:'',
-address:'',
-contact:'',
-docId:'',
+            <Text style={styles.label}>Last Name </Text>
+              <TextInput
+                style={styles.formTextInput}
+                placeholder={"Last Name"}
+                maxLength={12}
+                onChangeText={(text) => {
+                  this.setState({
+                    lastName: text,
+                  });
+                }}
+                value={this.state.lastName}
+              />
 
-}
-}
+                <Text style={styles.label}>Contact </Text>
+              <TextInput
+                style={styles.formTextInput}
+                placeholder={"Contact"}
+                maxLength={10}
+                keyboardType={"numeric"}
+                onChangeText={(text) => {
+                  this.setState({
+                    contact: text,
+                  });
+                }}
+                value={this.state.contact}
+              />
 
-getUserDetails=()=>{ 
-  var email = firebase.auth().currentUser.email; 
-  db.collection('users')
-  .where('email_id','==',email).get() 
-  .then(snapshot => { 
-    snapshot.forEach(doc => { 
-      var data = doc.data() 
-      console.log(data) 
-      this.setState({ 
-        emailId : data.email_id, 
-        firstName : data.first_name, 
-        lastName : data.last_name, 
-        address : data.address, 
-        contact : data.contact,
-         docId : doc.id 
-        }) 
-        }); 
-      }) 
-    }
-updateUserDetails=()=>{
-  db.collection('users').doc(this.state.docId).update({
-    first_name:this.state.firstName,
-    last_name:this.state.lastName,
-    contact:this.state.contact,
-    address:this.state.address,
-  })
-  alert("profile Updated successfuly")
-}
-
-
-
-componentDidMount(){
-  this.getUserDetails()
-}
-
-render(){
-return(
-<View  style={styles.container}>
-<Text style={{fontSize:30}}>Settings</Text>
-<View style={styles.formContainer}>
-<KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
-<TextInput
-style={styles.formTextInput}
-placeholder ={"First Name"}
-maxLength ={8}
-onChangeText={(text)=>{
-this.setState({
-firstName: text
-})
-}}
-value={this.state.firstName}
-/>
-<TextInput
-style={styles.formTextInput}
-placeholder ={"Last Name"}
-maxLength ={8}
-onChangeText={(text)=>{
-this.setState({
-lastName: text
-})
-}}
-value={this.state.lastName}
-/>
-<TextInput
-style={styles.formTextInput}
-placeholder ={"Contact"}
-maxLength ={10}
-keyboardType={'numeric'}
-onChangeText={(text)=>{
-this.setState({
-contact: text
-})
-}}
-value={this.state.contact}
-/>
-<TextInput
-style={styles.formTextInput}
-placeholder ={"Address"}
-multiline = {true}
-onChangeText={(text)=>{
-this.setState({
-address: text
-})
-}}
-value={this.state.address}
-
-/>
-
-<TouchableOpacity
-style={styles.save}
-onPress={()=>
-this.updateUserDetails()
-}
->
-<Text style={styles.saveText}>Save</Text>
-</TouchableOpacity>
-
-
-</KeyboardAvoidingView>
-
-</View>   
-</View>
-)
-}
+                <Text style={styles.label}>Address </Text>
+              <TextInput
+                style={styles.formTextInput}
+                placeholder={"Address"}
+                multiline
+                maxLength={10}
+                onChangeText={(text) => {
+                  this.setState({
+                    address: text,
+                  });
+                }}
+                value={this.state.address}
+              />
+            </View>
+              <View style={styles.buttonView}>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => {
+                    this.updateUserDetails();
+                  }}
+                >
+                  <Text style={styles.buttonText}>Save</Text>
+                </TouchableOpacity>
+            </View>
+          </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-save:{
-width:200,
-height:40,
-alignItems:'center',
-justifyContent:'center',
-borderWidth:1,
-borderRadius:10,
-marginTop:30,
-marginLeft:10
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor:"#6fc0b8"
+  },
+  formContainer:{
+    flex: 0.88,
+    justifyContent:'center'
+  },
+  label:{
+    fontSize:RFValue(18),
+    color:"#717D7E",
+    fontWeight:'bold',
+    padding:RFValue(10),
+    marginLeft:RFValue(20)
+  },
+  formTextInput: {
+    width: "90%",
+    height: RFValue(50),
+    padding: RFValue(10),
+    borderWidth:1,
+    borderRadius:2,
+    borderColor:"grey",
+    marginBottom:RFValue(20),
+    marginLeft:RFValue(20)
+  },
+  button: {
+    width: "75%",
+    height: RFValue(60),
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: RFValue(50),
+    backgroundColor: "#663399",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+    marginTop: RFValue(20),
+  },
+  buttonView:{
+    flex: 0.22,
+    alignItems: "center",
+    marginTop:RFValue(100)
 },
-saveText:{
-color:'#ff5722',
-fontSize:15,
-fontWeight:'bold',
-alignItems:'center',
-justifyContent:'center',
-alignSelf:'center',
-},
-formTextInput:{
-   width:"75%",
-   height:35,
-   alignSelf:'center',
-   borderColor:'#ffab91',
-   borderRadius:10,
-   borderWidth:1,
-   marginTop:20,
-   padding:10
- },
- KeyboardAvoidingView:{
-  flex:1,
-  justifyContent:'center',
-  alignItems:'center'
-},
-container : { 
-  flex:1, 
-  alignItems: 'center', 
-  justifyContent: 'center'
- }, 
-formContainer:{ 
-  flex:1, 
-  width:'100%', 
-  alignItems: 'center'
- },
-})
-
+  buttonText: {
+    fontSize: RFValue(23),
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
